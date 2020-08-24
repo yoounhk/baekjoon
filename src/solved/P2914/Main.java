d9973b344c88e2266963edd91e74109cf1d69f99
Yoounhk <yooun.hk@gmail.com>
2020-08-24 15:44:22 +0900

    2914 해결

---
+++ src/solved/P2914/Main.java
@@ -0,0 +1,14 @@
+package P2914;
+
+import java.util.Scanner;
+
+public class Main {
+  private static Scanner scanner = new Scanner(System.in);
+
+  public static void main(String args[]) {
+    int song = scanner.nextInt();
+    int melody = scanner.nextInt();
+    System.out.println(song * (melody - 1) + 1);
+    scanner.close();
+  }
+}
