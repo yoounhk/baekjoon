4fb58d6caa85eb950bd8a25a658a9c09c4f6addb
Yoounhk <yooun.hk@gmail.com>
2020-08-24 15:44:30 +0900

    3046 해결

---
+++ src/solved/P3046/Main.java
@@ -0,0 +1,15 @@
+package P3046;
+
+import java.util.Scanner;
+
+public class Main {
+  private static Scanner scanner = new Scanner(System.in);
+
+  public static void main(String args[]) {
+    int r1, s;
+    r1 = scanner.nextInt();
+    s = scanner.nextInt();
+    System.out.println(s * 2 - r1);
+    scanner.close();
+  }
+}
