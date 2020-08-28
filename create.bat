53842cc4680b17069c556f0acaaeb4b74c205948
HK Yooun <yooun.hk@gmail.com>
2020-08-28 22:24:03 +0900

    소스코드 템플릿 자동화 스크립트 작성

---
+++ create.bat
@@ -0,0 +1,13 @@
+@echo off
+SetLocal EnableDelayedExpansion
+set ClassName=%1%
+    echo package src; > src\%ClassName%.java
+    echo import java.util.Scanner; >> src\%ClassName%.java
+    echo public class %ClassName% { >> src\%ClassName%.java
+    echo private static Scanner scanner = new Scanner(System.in); >> src\%ClassName%.java
+    echo public static void main(String args[]) { >> src\%ClassName%.java
+    echo scanner.close(); >> src\%ClassName%.java
+    echo   } >> src\%ClassName%.java
+    echo } >> src\%ClassName%.java
+code src\%ClassName%.java
+EndLocal
