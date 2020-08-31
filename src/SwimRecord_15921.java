6faa82a0175058c0d2679dddae6c1adbb5e5fa2c
HK Yooun <yooun.hk@gmail.com>
2020-08-31 21:18:33 +0900

    15921번 해결

---
+++ src/SwimRecord_15921.java
@@ -0,0 +1,19 @@
+// https://github.com/yoounhk/baekjoon 
+package src;
+
+import java.util.Scanner;
+
+public class SwimRecord_15921 {
+	private static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String args[]) {
+		int sample = scanner.nextInt();
+		if (sample == 0) {
+			System.out.println("divide by zero");
+			scanner.close();
+			return;
+		}
+		System.out.println("1.00");
+		scanner.close();
+	}
+}
