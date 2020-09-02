a788eb0210bb9286ce84d0866b1811b5d008ff2d
HK Yooun <yooun.hk@gmail.com>
2020-09-02 11:06:23 +0900

    2480번 해결

---
+++ src/Dice_2480.java
@@ -0,0 +1,36 @@
+// https://github.com/yoounhk/baekjoon
+package src;
+
+import java.util.Scanner;
+
+public class Dice_2480 {
+	private static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String args[]) {
+		int[] dice = new int[7];
+		int[] input = { scanner.nextInt(), scanner.nextInt(), scanner.nextInt() };
+		int reward = 0;
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < input.length; i++) {
+			dice[input[i]] += 1;
+		}
+		for (int i = 0; i < dice.length; i++) {
+			if (dice[i] == 3) {
+				reward = 10000 + (i * 1000);
+			} else if (dice[i] == 2) {
+				reward = 1000 + (i * 100);
+			} else if (dice[i] != 0) {
+				max = max(i, max);
+			}
+		}
+		if (reward == 0) {
+			reward = max * 100;
+		}
+		System.out.println(reward);
+		scanner.close();
+	}
+
+	private static int max(int i, int j) {
+		return i > j ? i : j;
+	}
+}
