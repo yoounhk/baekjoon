9f0c60177ab4779ee24e2816c2733c33070d6b57
HK Yooun <yooun.hk@gmail.com>
2020-09-22 20:38:12 +0900

    Solve 3460

--- src/Binary_3460.java
+++ src/Binary_3460.java
@@ -1,26 +1,26 @@
 package src;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Binary_3460 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int testCase = sc.nextInt();
+        String[] outputs = new String[testCase];
         for (int i = 0; i < testCase; i++) {
             String binary = Integer.toBinaryString(sc.nextInt());
-            ArrayList<Integer> idxOfOne = new ArrayList<>();
-            for (int j = 0; j < binary.length(); j++)
-                if (binary.charAt(j) == '1')
-                    idxOfOne.add(j);
-            for (int j = 0; j < idxOfOne.size(); j++) {
-                for (int k = 0; k < idxOfOne.size() - j; k++) {
-                    // TODO: WIP 정렬 + 인덱스 매핑
-                }
+            int[] digit = new int[binary.length()];
+            outputs[i] = "";
+            for (int j = 0; j < binary.length(); j++) {
+                int x = binary.length() - 1 - j;
+                digit[x] = binary.charAt(j) == '1' ? 1 : 0;
+                if (digit[x] == 1) outputs[i] += x+" ";
             }
-            for (int j = 0; j < idxOfOne.size(); j++) {
-                System.out.printf("%d ", idxOfOne.get(j));
+            String[] output = outputs[i].trim().split(" ");
+            for (int j = output.length - 1; j >= 0; j--) {
+                System.out.print(output[j]+" ");
             }
+            System.out.println();
         }
     }
 }
