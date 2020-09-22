0a729285f7539eaf807d17492ab221c8c0b32a6d
HK Yooun <yooun.hk@gmail.com>
2020-09-22 21:05:49 +0900

    Init wip branch

---
+++ WIP/P15740/SecondTry.java
@@ -0,0 +1,17 @@
+package src;
+
+import java.util.Scanner;
+
+public class Main {
+  private static Scanner scanner = new Scanner(System.in);
+
+  public static void main(String args[]) {
+    // String str = scanner.next();
+    // String[] array = str.split(" ");
+    // int[] n = Input.toIntegerArray(str);
+    // scanner.close();
+    System.out.println(Long.MAX_VALUE);
+    System.out.println(Long.MIN_VALUE);
+
+  }
+}
