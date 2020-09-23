ae0fc2d41feebcb4e4c8d3302ad68ce97f27bc85
yhk <ezenyhk@gmail.com>
2020-09-23 17:50:35 +0900

    Refactor 2511

--- refactoring_needed/CardGame_2511.java
+++ refactoring_needed/CardGame_2511.java
@@ -1,69 +1,36 @@
 // https://github.com/yoounhk/baekjoon
-package src;
+package refactoring_needed;
 
 import java.util.Scanner;
 
 public class CardGame_2511 {
-	private static Scanner scanner = new Scanner(System.in);
-
-	public static void main(String args[]) {
-		Card a = new Card(toIntegerArray(scanner.nextLine()));
-		Card b = new Card(toIntegerArray(scanner.nextLine()));
-		Card.play(a, b);
-		String winner = Card.getWinner();
-		int[] scores = Card.getMatchScore();
-		System.out.println(scores[0] + " " + scores[1]);
-		System.out.println(winner);
-	}
-
-	private static int[] toIntegerArray(String str) {
-		String[] array = str.split(" ");
-		int[] result = new int[array.length];
-		for (int i = 0; i < array.length; i++) {
-			result[i] = Integer.parseInt(array[i]);
-		}
-		return result;
-	}
-}
-
-class Card {
-	private static final int MATCH_SIZE = 10;
-	private static String latestWinner = "D";
-	private static String winner = "";
-	private static int[] scores = { 0, 0 };
-	private int[] cards = new int[MATCH_SIZE];
-
-	public Card(int[] cards) {
-		this.cards = cards;
-	}
-
-	public static void play(Card a, Card b) {
-		for (int i = 0; i < MATCH_SIZE; i++) {
-			if (a.cards[i] > b.cards[i]) {
-				scores[0] += 3;
-				latestWinner = "A";
-			} else if (a.cards[i] < b.cards[i]) {
-				scores[1] += 3;
-				latestWinner = "B";
-			} else if (a.cards[i] == b.cards[i]) {
-				scores[0] += 1;
-				scores[1] += 1;
-			}
-			if (scores[0] > scores[1]) {
-				winner = "A";
-			} else if (scores[0] < scores[1]) {
-				winner = "B";
-			} else if (scores[0] == scores[1]) {
-				winner = latestWinner;
-			}
-		}
-	}
-
-	public static int[] getMatchScore() {
-		return new int[] { scores[0], scores[1] };
-	}
-
-	public static String getWinner() {
-		return winner;
-	}
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int round = 10;
+        int aScore = 0;
+        int bScore = 0;
+		String[] a = sc.nextLine().split(" ");
+		String[] b = sc.nextLine().split(" ");
+        for (int i = 0; i < round; i++) {
+            if (Integer.parseInt(a[i]) > Integer.parseInt(b[i])) aScore += 3;
+            else if (Integer.parseInt(a[i]) < Integer.parseInt(b[i])) bScore += 3;
+            else {
+                aScore++;
+                bScore++;
+            }
+        }
+        System.out.println(aScore + " " + bScore);
+        char winner = 'D';
+        if (aScore > bScore) winner = 'A';
+        else if (aScore < bScore) winner = 'B';
+        else {
+            for (int i = round - 1; i >= 0; i--) {
+                if (!a[i].equals(b[i])) {
+                    winner = Integer.parseInt(a[i]) > Integer.parseInt(b[i]) ? 'A' : 'B';
+                    break;
+                }
+            }
+        }
+        System.out.println(winner);
+    }
 }
