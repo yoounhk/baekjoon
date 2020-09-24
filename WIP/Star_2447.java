3b3c86e53d7d3292458e32d7dc1c3805eee4e18a
HK Yooun <yooun.hk@gmail.com>
2020-09-24 10:34:58 +0900

    Commit 2447 branch

---
+++ WIP/Star_2447.java
@@ -0,0 +1,12 @@
+package WIP;
+
+import java.util.Scanner;
+
+public class Star_2447 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+            // TODO
+        }
+    }
+}
